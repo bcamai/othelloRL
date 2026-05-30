683081336e428b440565df79a3e9fff88988aa05
Bartosz <bartosz.kamajj@gmail.com>
2026-05-30 17:52:10 +0200

    Added FindLegalMoves

--- game/othello.cpp
+++ game/othello.cpp
@@ -8,6 +8,9 @@ typedef unsigned int uint32;
 #define NOT_UPPER_ROW 0x00FFFFFFFFFFFFFFllu
 #define NOT_LOWER_ROW 0xFFFFFFFFFFFFFF00llu
 
+#define WHITE 1
+#define BLACK 0
+
 // bit 0 H8 bit 63 A1
 void 
 PrintBoard(u64 Black, u64 White)
@@ -53,16 +56,16 @@ GetScore(u64 Board)
 
 
 void
-ApplyMove(u64 Black, u64 White, 
+ApplyMove(u64 BlackBoard, u64 WhiteBoard, 
 		bool CurrentColor, u64 Move,
-		u64* BlackAfter, u64* WhiteAfter)
+		u64 *BlackAfter, u64 *WhiteAfter)
 {
-	u64 CurrentBoard = Black;
-	u64 OpponentsBoard = White;
+	u64 CurrentBoard = BlackBoard;
+	u64 OpponentsBoard = WhiteBoard;
 	if(CurrentColor)
 	{
-		CurrentBoard = White;
-		OpponentsBoard = Black;
+		CurrentBoard = WhiteBoard;
+		OpponentsBoard = BlackBoard;
 	}
 
 	u64 ToSwap = 0, ToSwapTemp = 0, Position;
@@ -178,23 +181,118 @@ ApplyMove(u64 Black, u64 White,
 	}
 }
 
+	void
+FindValidMoves(u64 BlackBoard, u64 WhiteBoard,
+							 bool CurrentColor, u64 *Moves)
+{
+	u64 CurrentBoard = BlackBoard;
+	u64 OpponentsBoard = WhiteBoard;
+	if(CurrentColor)
+	{
+		CurrentBoard = WhiteBoard;
+		OpponentsBoard = BlackBoard;
+	}
+
+	u64 Position, Empty, ValidMoves = 0;
+
+	Empty = ~(CurrentBoard | OpponentsBoard);
+
+	// Left
+	Position = OpponentsBoard & ((CurrentBoard << 1) & NOT_LEFT_COL);
+	while(Position)
+	{
+		ValidMoves = ValidMoves | (Empty & (Position << 1 & NOT_LEFT_COL));
+		Position = OpponentsBoard & ((Position << 1) & NOT_LEFT_COL);
+	}
+	// Right
+	Position = OpponentsBoard & ((CurrentBoard >> 1) & NOT_RIGHT_COL);
+	while(Position)
+	{
+		ValidMoves = ValidMoves | (Empty & (Position >> 1 & NOT_RIGHT_COL));
+		Position = OpponentsBoard & ((Position >> 1) & NOT_RIGHT_COL);
+	}
+	// Up
+	Position = OpponentsBoard & ((CurrentBoard << 8) & NOT_UPPER_ROW);
+	while(Position)
+	{
+		ValidMoves = ValidMoves | (Empty & (Position << 8 & NOT_UPPER_ROW));
+		Position = OpponentsBoard & ((Position << 8) & NOT_UPPER_ROW);
+	}
+	// Down
+	Position = OpponentsBoard & ((CurrentBoard >> 8) & NOT_LOWER_ROW);
+	while(Position)
+	{
+		ValidMoves = ValidMoves | (Empty & (Position >> 8 & NOT_LOWER_ROW));
+		Position = OpponentsBoard & ((Position >> 8) & NOT_LOWER_ROW);
+	}
+	// Upper Left
+	Position = OpponentsBoard & ((CurrentBoard << 1 << 8) & NOT_LEFT_COL & NOT_UPPER_ROW);
+	while(Position)
+	{
+		ValidMoves = ValidMoves | (Empty & ((Position << 1 << 8) & NOT_UPPER_ROW));
+		Position = OpponentsBoard & ((Position << 1 << 8) & NOT_LEFT_COL & NOT_UPPER_ROW);
+	}
+	// Upper Right
+	Position = OpponentsBoard & ((CurrentBoard >> 1 << 8) & NOT_RIGHT_COL & NOT_UPPER_ROW);
+	while(Position)
+	{
+		ValidMoves = ValidMoves | (Empty & ((Position >> 1 << 8) & NOT_RIGHT_COL & NOT_UPPER_ROW));
+		Position = OpponentsBoard & ((Position >> 1 << 8) & NOT_RIGHT_COL & NOT_UPPER_ROW);
+	}
+	// Lower Left
+	Position = OpponentsBoard & ((CurrentBoard << 1 >> 8) & NOT_LEFT_COL & NOT_LOWER_ROW);
+	while(Position)
+	{
+		ValidMoves = ValidMoves | (Empty & ((Position << 1 >> 8) & NOT_LEFT_COL & NOT_LOWER_ROW));
+		Position = OpponentsBoard & ((Position << 1 >> 8) & NOT_LEFT_COL & NOT_LOWER_ROW);
+	}
+	// Lower Right
+	Position = OpponentsBoard & ((CurrentBoard >> 1 >> 8) & NOT_RIGHT_COL & NOT_LOWER_ROW);
+	while(Position)
+	{
+		ValidMoves = ValidMoves | (Empty & ((Position >> 1 >> 8) & NOT_RIGHT_COL & NOT_LOWER_ROW));
+		Position = OpponentsBoard & ((Position >> 1 >> 8) & NOT_RIGHT_COL & NOT_LOWER_ROW);
+	}
+
+	*Moves = ValidMoves;
+}
+
+
 int
 main() {
 	u64 BlackBoard = (1ULL << 28) | (1ULL << 35);
 	u64 WhiteBoard = (1ULL << 27) | (1ULL << 36);
 
-	printf("Init: \n");
+	int WhiteScore = GetScore(WhiteBoard);
+	int BlackScore = GetScore(BlackBoard);
+
+	printf("Init: %i %i \n", WhiteScore, BlackScore);
+	u64 ValidMoves;
+	FindValidMoves(BlackBoard, WhiteBoard,
+								 BLACK, &ValidMoves);
 	PrintBoard(BlackBoard, WhiteBoard);
-	printf("Test move \n");
+	PrintBoard(ValidMoves, 0ull);
+	
 	ApplyMove(BlackBoard, WhiteBoard,
 			0, 1ULL << 26,
 			&BlackBoard, &WhiteBoard);
+	WhiteScore = GetScore(WhiteBoard);
+	BlackScore = GetScore(BlackBoard);	
+	printf("Test move 1. %i %i \n", WhiteScore, BlackScore);
+	FindValidMoves(BlackBoard, WhiteBoard,
+								 WHITE, &ValidMoves);
 	PrintBoard(BlackBoard, WhiteBoard);
-	printf("Test move 2 \n");
+	PrintBoard(0ull, ValidMoves);
+	
 	ApplyMove(BlackBoard, WhiteBoard,
 			1, 1ULL << 20,
 			&BlackBoard, &WhiteBoard);
+	WhiteScore = GetScore(WhiteBoard);
+	BlackScore = GetScore(BlackBoard);	
+	printf("Test move 2. %i %i \n", WhiteScore, BlackScore);
+	FindValidMoves(BlackBoard, WhiteBoard,
+								 BLACK, &ValidMoves);
 	PrintBoard(BlackBoard, WhiteBoard);
-
+	PrintBoard(ValidMoves, 0ull);
 	return 0;
 }
